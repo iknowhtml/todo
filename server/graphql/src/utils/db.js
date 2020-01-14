2a42d53e6ecb6c9ebd797ce1b4917a655e1ea27e
iknowhtml <iknowsmtp@gmail.com>
2020-01-14 14:21:16 -0500

    refactor(graphql): Update db connection module
    Remove version from base configuration,
    add logic for production environment database connection,
    refactor code to return async function rather than connected db object

--- server/graphql/src/utils/db.js
+++ server/graphql/src/utils/db.js
@@ -1,27 +1,46 @@
 import knex from 'knex';
+import axios from 'axios';
 
 const baseConfig = {
   client: 'pg',
-  version: '7.17.1',
 };
 
-let config;
-if (process.env.ENVIRONMENT === 'development') {
-  const developmentConfig = {
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-  };
+const connect = async () => {
+  let config;
+  if (process.env.ENVIRONMENT === 'development') {
+    const developmentConfig = {
+      connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      },
+    };
 
-  config = { ...baseConfig, ...developmentConfig };
-} else {
-  const productionConfig = {};
-  config = { ...baseConfig, ...productionConfig };
-}
+    config = { ...baseConfig, ...developmentConfig };
+  } else {
+    const herokuApi = axios.create({
+      baseURL: 'https://api.heroku.com/',
+      headers: {
+        Authorization: `Bearer ${process.env.HEROKU_API_KEY}`,
+        Accept: 'application/vnd.heroku+json; version=3',
+      },
+    });
 
-const client = knex(config);
+    const response = await herokuApi.get(
+      `addons/${process.env.HEROKU_POSTGRES_ID}/config`
+    );
 
-export default client;
+    const productionConfig = {
+      connection: `${response.data[0].value}?ssl=true`,
+    };
+
+    config = { ...baseConfig, ...productionConfig };
+  }
+
+  const client = knex(config);
+
+  return client;
+};
+
+export default { connect };

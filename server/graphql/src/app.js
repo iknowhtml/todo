9681ee8c0b10741314d09de0ce99b3c7d6d3582a
iknowhtml <iknowsmtp@gmail.com>
2020-01-14 14:23:54 -0500

    fix(graphql): Update placeholder route logic
    Update placeholder route logic to return the current time of the database
    the application is connected

--- server/graphql/src/app.js
+++ server/graphql/src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import client from './utils/db';
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send(process.env.PSQL_DB);
+app.get('/', async (req, res) => {
+  const db = await client.connect();
+  const response = await db.raw('SELECT now()');
+  res.send(response.rows[0].now);
 });
 
 export default app;

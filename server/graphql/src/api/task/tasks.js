0050e4177cdf8626b4f40a352f832407b2737c5d
iknowhtml <iknowsmtp@gmail.com>
2020-01-15 12:42:50 -0500

    refactor(graphql): Add tasks data

---
+++ server/graphql/src/api/task/tasks.js
@@ -0,0 +1,24 @@
+const tasks = {
+  1: {
+    name: 'task 1',
+    description: 'description 1',
+    completed: false,
+  },
+  2: {
+    name: 'task 2',
+    description: 'description 2',
+    completed: false,
+  },
+  3: {
+    name: 'task 3',
+    description: 'description 3',
+    completed: false,
+  },
+  4: {
+    name: 'task 4',
+    description: 'description 4',
+    completed: false,
+  },
+};
+
+export default tasks;

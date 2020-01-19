582f2f232a4ab5ec8674f546d2365f4a88c94562
iknowhtml <iknowsmtp@gmail.com>
2020-01-19 17:09:28 -0500

    Refactor(graphql): Add User schema

---
+++ server/graphql/src/api/user/schemas.js
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-server-express';
+
+const schema = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    first_name: String!
+    last_name: String!
+  }
+
+  type Token {
+    token: String!
+  }
+
+  extend type Mutation {
+    signIn(username: String!, password: String!): Token
+  }
+`;
+
+export default schema;

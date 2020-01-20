ad0b7eafbd72fba5c76f5f8fb26fb8a2d75b089b
iknowhtml <iknowsmtp@gmail.com>
2020-01-20 11:33:04 -0500

    chore(graphql): Update ESLint configuration

--- server/graphql/.eslintrc.js
+++ server/graphql/.eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
+  parser: 'babel-eslint',
   parserOptions: {
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: ['prettier'],
+  env: {
+    node: true,
+    jest: true,
+  },
+  plugins: ['babel', 'prettier'],
   extends: ['eslint:recommended'],
   rules: {
     'prettier/prettier': ['error'],
     'no-console': 0,
     'comma-dangle': ['error', 'only-multiline'],
-  },
-  env: {
-    es2020: true,
-    node: true,
-    jest: true,
   },
 };

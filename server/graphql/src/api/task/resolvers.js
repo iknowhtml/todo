87eeb80bc9191081bf23c6d99f0780799f549227
iknowhtml <iknowsmtp@gmail.com>
2020-01-20 21:59:04 -0500

    refactor(graphql): Pass id into task methods

--- server/graphql/src/api/task/resolvers.js
+++ server/graphql/src/api/task/resolvers.js
@@ -3,13 +3,16 @@ import authenticate from '../utils/authenticate';
 
 const resolvers = {
   Query: {
-    tasks: async () => await Task.getAll(),
-    task: async (_, { id }) => await Task.get(id),
+    tasks: async (_, __, { user_id }) => await Task.getAll(user_id),
+    task: async (_, { id }, { user_id }) => await Task.getById(id, user_id),
   },
   Mutation: {
-    createTask: async (_, { name }) => await Task.create(name),
-    removeTask: async (_, { id }) => await Task.remove(id),
-    updateTask: async (_, { id, ...values }) => await Task.update(id, values),
+    createTask: async (_, { name }, { user_id }) =>
+      await Task.create(name, user_id),
+    removeTask: async (_, { id }, { user_id }) =>
+      await Task.removeById(id, user_id),
+    updateTask: async (_, { id, ...values }, { user_id }) =>
+      await Task.updateById(id, values, user_id),
   },
 };
 

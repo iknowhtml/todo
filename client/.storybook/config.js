a8c13fd78471859d31257751027f1cea2282de63
iknowhtml <iknowsmtp@gmail.com>
2020-01-25 19:48:02 -0500

    chore(client): Remove extraneous configurations

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,20 +1,10 @@
 import { configure, addDecorator } from '@storybook/react';
 import React from 'react';
 import GlobalStyles from '../src/components/GlobalStyles';
-const req = require.context('../src', true, /.stories.js$/);
 
-const loadStories = () => {
-  for (const filename of req.keys()) {
-    req.keys().forEach(filename => req(filename));
-  }
-};
-
-const withGlobal = cb => (
+addDecorator(component => (
   <>
     <GlobalStyles />
-    {cb()}
+    {component()}
   </>
-);
-
-addDecorator(withGlobal);
-configure(loadStories, module);
+));

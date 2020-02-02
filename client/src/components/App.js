8a7f615c1c178e5fa434e31263e2d0167e8f2dc6
iknowhtml <iknowsmtp@gmail.com>
2020-02-02 12:45:14 -0500

    refactor(client): Rename file extension
    Change from .js to .jsx

--- client/src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import styled from '@emotion/styled';
-
-const App = () => <Text>Hello World</Text>;
-
-export default App;
---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from './Common/Text';
+
+const App = () => (
+  <View>
+    <Text>Hello World</Text>
+  </View>
+);
+
+export default App;

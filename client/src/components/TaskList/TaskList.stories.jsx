c8265a8cf587c00030f96051c9c827cfda596845
iknowhtml <iknowsmtp@gmail.com>
2020-02-03 20:57:22 -0500

    refactor(client): Update data for TaskList stories

--- client/src/components/TaskList/TaskList.stories.jsx
+++ client/src/components/TaskList/TaskList.stories.jsx
@@ -11,19 +11,18 @@ const tasks = [
     id: 'b4e893e5-f7da-48fa-8331-c88215afb984',
     name: 'task 1',
     completed: false,
-    due_date: null,
+    dueTime: '17:00',
   },
   {
     id: 'ea4baa0f-1321-46aa-82b9-c58d1b8e3a3e',
     name: 'task 2',
     completed: true,
-    due_date: null,
   },
   {
     id: 'eea2549e-b548-43cb-9c87-4e13fa71141a',
     name: 'task 3',
     completed: false,
-    due_date: null,
+    selected: true,
   },
 ];
 

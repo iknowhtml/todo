e3655c86b677b9b8e697e86d5ee13c3d3fa41385
iknowhtml <iknowsmtp@gmail.com>
2020-02-08 21:26:21 -0500

    refactor(client): Remove unnecessary props

--- client/src/components/TaskList/Task/Task.stories.jsx
+++ client/src/components/TaskList/Task/Task.stories.jsx
@@ -6,32 +6,26 @@ export default {
   component: Task,
 };
 
-export const incompleteTask = () => (
-  <Task completed={false}>Incomplete Task</Task>
-);
+export const incompleteTask = () => <Task>Incomplete Task</Task>;
 
 export const incompleteTaskWithDueTime = () => (
-  <Task completed={false} dueTime={'17:00'}>
-    Incomplete Task with Due Time
-  </Task>
+  <Task dueTime={'17:00'}>Incomplete Task with Due Time</Task>
 );
 
-export const completedTask = () => <Task completed={true}>Completed Task</Task>;
+export const completedTask = () => <Task completed>Completed Task</Task>;
 
 export const selectedIncompleteTask = () => (
-  <Task completed={false} selected={true}>
-    Incomplete Task
-  </Task>
+  <Task selected>Incomplete Task</Task>
 );
 
 export const selectedIncompleteTaskWithDueTime = () => (
-  <Task completed={false} selected={true} dueTime={'17:00'}>
-    Selected Incomplete Task with Due Time
+  <Task completed selected dueTime={'17:00'}>
+    selected Incomplete Task with Due Time
   </Task>
 );
 
 export const selectedCompletedTaskWithDueTime = () => (
-  <Task completed={true} selected={true} dueTime={'17:00'}>
-    Selected Completed Task
+  <Task completed selected dueTime={'17:00'}>
+    selected Completed Task
   </Task>
 );

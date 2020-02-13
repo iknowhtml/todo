1919fb51d87d5a332f80eaa10ae53bf199235b9c
iknowhtml <iknowsmtp@gmail.com>
2020-02-12 21:10:44 -0500

    refactor(client): Extract TaskListHeader component Extract TaskListHeader component to separate file from TaskList

--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -1,50 +1,13 @@
 import React from 'react';
 import styled, { css } from '@emotion/native';
-import { format, parseISO } from 'date-fns';
 
-import Text from '../Common/Text';
 import Task from './Task';
+import TaskListHeader from './TaskListHeader';
 
 const TaskListContainer = styled.View`
   display: flex;
   background: white;
 `;
-
-const TaskListHeaderContainer = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: ${({ theme }) => theme.taskHorizontalPadding};
-`;
-
-const DayOfWeekHeader = styled(Text)`
-  font-size: 1.1rem;
-  font-weight: 600;
-`;
-
-const DateHeader = styled(Text)`
-  font-size: 0.7rem;
-  color: ${({ theme }) => theme.secondaryColor};
-  margin-left: auto;
-`;
-
-const TaskListHeader = ({ date, today = format(new Date(), 'yyyy-MM-dd') }) => {
-  const isToday = date === today;
-  const dateObject = parseISO(date);
-  const dayOfWeek = format(dateObject, 'EEEE');
-  const monthDay = format(dateObject, 'MMMM do');
-
-  return (
-    <TaskListHeaderContainer>
-      <DayOfWeekHeader>
-        {isToday ? 'today' : dayOfWeek.toLowerCase()}
-      </DayOfWeekHeader>
-      <DateHeader>
-        {(isToday ? `${dayOfWeek},  ${monthDay}` : `${monthDay}`).toUpperCase()}
-      </DateHeader>
-    </TaskListHeaderContainer>
-  );
-};
 
 const TaskList = ({ tasks, date }) => {
   return (
---
+++ client/src/components/TaskList/TaskListHeader.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from '@emotion/native';
+import { format, parseISO } from 'date-fns';
+import Text from '../Common/Text';
+
+const TaskListHeaderContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: ${({ theme }) => theme.taskHorizontalPadding};
+`;
+
+const DayOfWeekHeader = styled(Text)`
+  font-size: 1.1rem;
+  font-weight: 600;
+`;
+
+const DateHeader = styled(Text)`
+  font-size: 0.7rem;
+  color: ${({ theme }) => theme.secondaryColor};
+  margin-left: auto;
+`;
+
+const TaskListHeader = ({ date, today = format(new Date(), 'yyyy-MM-dd') }) => {
+  const isToday = date === today;
+  const dateObject = parseISO(date);
+  const dayOfWeek = format(dateObject, 'EEEE');
+  const monthDay = format(dateObject, 'MMMM do');
+  return (<TaskListHeaderContainer>
+    <DayOfWeekHeader>
+      {isToday ? 'today' : dayOfWeek.toLowerCase()}
+    </DayOfWeekHeader>
+    <DateHeader>
+      {(isToday ? `${dayOfWeek},  ${monthDay}` : `${monthDay}`).toUpperCase()}
+    </DateHeader>
+  </TaskListHeaderContainer>);
+};
+
+export degault TaskListHeader;
